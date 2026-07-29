1c7631c077adb8f325f7d85c60acc93dbc2ec9ca
Vicente <vicente@per>
2026-07-29 08:26:40 +0200

    Optimizar alpine headless

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -12,7 +12,7 @@ const val MainClass = "suwayomi.tachidesk.MainKt"
 // should be bumped with each stable release
 val getTachideskVersion = { "v2.3.${getCommitCount()}" }
 
-val webUIRevisionTag = "r3831"
+val webUIRevisionTag = "r3862"
 
 private val getCommitCount = {
     runCatching {
